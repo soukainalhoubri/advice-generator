9136fd45d67f508c72bdd1e668a1033cd200a82b
souka2000 <soukainalhoubri@gmail.com>
2022-07-12 01:22:38 +0100

    fetch function

--- advice.js
+++ advice.js
@@ -1,22 +1,21 @@
-const adviceId=document.getElementById("advice-id");
-const advice=document.getElementById("advice");
-const dice=document.getElementById("dice");
+var adviceId=document.getElementById("advice-id");
+var paragraph=document.getElementById("advice");
+var dice=document.getElementById("dice");
 
 window.onload = showQuote;
 
-dice.addEventListener("click", function(){
-    showQuote();
-});
-
 function showQuote(){
-    fetch("https://api.adviceslip.com/advice")
+    
+    fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then((data) => data.slip)
     .then((data) => {
-     adviceId.style.display="None";
-    //  advice.textContent = data.advice;
+    adviceId.innerText=data.id
+    paragraph.innerText=data.advice
+    
     })
     .catch((error) => {
         alert(error);
     })
+	
 }
